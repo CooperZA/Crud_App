924b59433158b8157e41ce134162dc5e61b08d91
CooperZA <zach@raincitysolutions.com>
2019-11-27 20:25:36 -0800

    config add line

--- include/config.php
+++ include/config.php
@@ -2,6 +2,7 @@
 
 // hostname, username, password, db
 $con = mysqli_connect('localhost','zach', 'Zac4Admin!', 'registration');
+// $con = mysqli_connect('localhost','root', '', 'registration');
 
 if(!$con){
     echo 'Database Not Connected';
